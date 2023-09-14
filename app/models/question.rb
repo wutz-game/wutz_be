127fcad7cb731e487d14e73f71004dce2b4ce221
Chris Cullinane <chriscullinane@protonmail.com>
2023-09-13 19:06:07 -0800

    question model and tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  belongs_to :game
+  validates :clue, :answer, :category, :clue_id, :game_id, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:clue) }
+    it { should validate_presence_of(:answer) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:clue_id) }
+    it { should validate_presence_of(:game_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:game) }
+  end
 end
