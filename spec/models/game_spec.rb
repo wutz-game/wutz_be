5c5de44e59c38c5f35346644fe270cff0f218768
Chris Cullinane <chriscullinane@protonmail.com>
2023-09-13 19:05:56 -0800

    game model and tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:orig_date) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:questions) }
+  end
 end
