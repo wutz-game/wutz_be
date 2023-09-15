d452a0d58cf0fea1f78e386048b3764f3e660767
Chris Cullinane <chriscullinane@protonmail.com>
2023-09-15 10:36:53 -0800

    fix: factory data

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :question do
-    clue { "WTF" }
-    answer { "White Bread" }
-    category { ["IS", "THIS", "WORKING?"].sample  }
+    clue { "Hello" }
+    answer { "Hi" }
+    category { ["IS", "THIS", "WORKING?"].sample }
     value { rand(1..3) }
     category_id { rand(42..69) }
-    clue_id { 42 }
+    clue_id { 420 }
   end
 end
