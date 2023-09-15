6b10252a78efb628796846e64759a461ad63d5a1
Chris Cullinane <chriscullinane@protonmail.com>
2023-09-15 01:12:31 -0800

    GET #index test passes

--- spec/controllers/api/challenges_controller_spec.rb
+++ spec/controllers/api/challenges_controller_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe Api::ChallengesController, type: :controller do
   describe 'GET #index' do
     it 'returns the last 9 game questions as JSON' do
-      # Create a game
       game = FactoryBot.create(:game)
-
-      # Create 9 game_question records associated with the same game
       game_questions = FactoryBot.create_list(:game_question, 9, game: game)
 
-      # Make a GET request to the show action
       get :index
 
-      # Validate the response
       expect(response).to have_http_status(:ok)
       parsed_response = JSON.parse(response.body)
-      binding.pry
-      expect(parsed_response.size).to eq(9) # Ensure 9 game questions were returned
-      # Add more expectations based on your serialization format
+      
+      # Ensure there are 3 categories
+      expect(parsed_response["data"]["categories"].count).to eq(3)
+
+      # Ensure there are 9 questions in total
+      total_questions = parsed_response["data"]["categories"].sum { |category| category["questions"].count }
+      expect(total_questions).to eq(9)
+      # Add more expectations based on our serialization format
     end
   end
 end
