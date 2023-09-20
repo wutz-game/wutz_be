2c8e67ecf8050c68e5c88b83a143baa5a8758017
Ethan Black <ethanrossblack@gmail.com>
2023-09-20 11:37:37 -0600

    fix: question factory

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :question do
-    clue { "Hello" }
     answer { "Hi" }
-    category { ["IS", "THIS", "WORKING?"].sample }
-    value { rand(1..3) }
+    category { ["Category1", "category2", "category3"].sample }
     category_id { rand(42..69) }
+    clue { "Hello" }
     clue_id { 420 }
+    value { rand(1..3) }
   end
 end
