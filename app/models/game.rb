f83091409c645468888aa7ba0fb0bff35f45524d
Ethan Black <ethanrossblack@gmail.com>
2023-09-21 09:27:36 -0600

    feat: add relationship to QuesitonCategory

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ApplicationRecord
   has_many :questions, through: :game_questions
   has_many :user_games
   has_many :user_answers, through: :user_games
+  has_many :game_question_categories
+  has_many :question_categories, through: :game_question_categories
   
   validates :orig_date, presence: true
 end
